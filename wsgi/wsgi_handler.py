f148573bbab702455f6df29f055aaea7f5ff051a
paulb <paulb@fen-desktop6>
2011-05-24 18:04:54 +0100

    Further changes for deployment.
    Change-Id: I52d6de5fa310e5e12289a5e50b667475c2499b7b

--- wsgi/wsgi_handler.py
+++ wsgi/wsgi_handler.py
@@ -1,15 +1,15 @@
 import os, sys
 
 project_dir = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(os.path.abspath(os.path.join(project_dir, '..')))
-sys.path.append(os.path.abspath(os.path.join(project_dir, '../project_name/')))
+sys.path.append(os.path.abspath(os.path.join(project_dir, '../django')))
+sys.path.append(os.path.abspath(os.path.join(project_dir, '/django/openconsent/')))
 
 #print >> sys.stderr, sys.path
 
 #sys.path.append('/var/project_name-stage')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project_name.settings'
-os.environ['PROJECT_NAME_HOME'] = '/var/django/project_name/dev/'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'openconsent.settings'
+os.environ['OPENCONSENT_HOME'] = '/var/django/openconsent/dev/django/'
 
 import django.core.handlers.wsgi
 
