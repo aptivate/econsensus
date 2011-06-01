65ca2fa7097e750bf38e38804f855a5f3c670826
paulb <paulb@fen-desktop6>
2011-06-01 18:03:49 +0100

    Added hierarchy to group view.
    Change-Id: Iddea787a79b099a180af7d6294efcf1574fe40ca

--- django/openconsent/publicweb/admin.py
+++ django/openconsent/publicweb/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 from django.db import models
 from django import forms
+from django.shortcuts import get_object_or_404
+from django.forms import MediaDefiningClass
+
+from functools import partial
 
 from models import Decision, Group, Concern
+from views import DecisionTable
+
+
+class GroupAdmin(admin.ModelAdmin):
+    model = Group
+    
+    change_form_template = 'admin/group_change_form.html'
+    
+    def change_view(self, request, object_id, extra_context=None):
+                
+        group = get_object_or_404(Group, pk=object_id)
+        
+        decisions = group.decision_set.all()
+        
+        print "***Here's the decisions:", dict(decisions = decisions)
+
+        extra_context = {
+            'decisions': decisions,
+        }
+        return super(GroupAdmin, self).change_view(request, object_id,
+            extra_context=extra_context)
+    
 
 class ConcernInline(admin.TabularInline):
     model = Concern
@@ -15,21 +41,20 @@ class ConcernInline(admin.TabularInline):
         models.TextField: {'widget': forms.Textarea(attrs={'rows':5, 'cols':80})},
         }
 
-
-    
 class DecisionAdmin(admin.ModelAdmin):
 #    can't get nested fields to work...
 #    fields = ['short_name',('effective_date','decided_date','review_date')]
-
+        
     fieldsets = [
         (None, {'fields': ('short_name','description', 
-                           ('effective_date','decided_date','review_date','expiry_date'),
+                           ('effective_date','decided_date'),
+                           ('review_date','expiry_date'),
                            'budget','people','group')}),
     ]
 
     list_display = ('short_name','decided_date','effective_date','review_date','expiry_date','budget','people','group')
     search_fields = ('short_name',)
-    list_filter = ('decided_date','effective_date','review_date','expiry_date',)
+    list_filter = ('group','decided_date','effective_date','review_date',)
     inlines = (ConcernInline,)
     formfield_overrides = {
         models.CharField: {'widget': forms.TextInput(attrs={'size':'86'})},
@@ -38,6 +63,6 @@ class DecisionAdmin(admin.ModelAdmin):
 class ConcernAdmin(admin.ModelAdmin):
     list_display = ('short_name','description','resolved')
         
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Concern,ConcernAdmin)
 admin.site.register(Decision,DecisionAdmin)
