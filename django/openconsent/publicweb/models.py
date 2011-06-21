5e2c5fe9e22e601fe6be27fe0503b44520347365
paulb <paulb@fen-desktop6>
2011-06-21 18:16:59 +0100

    Working internationalisation test.
    Change-Id: I9da89642772af50c32efe80508166db64b50d869

--- django/openconsent/publicweb/models.py
+++ django/openconsent/publicweb/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 import tinymce.models
 import tinymce.widgets
 import datetime
--- django/openconsent/publicweb/tests/internationalisation_test.py
+++ django/openconsent/publicweb/tests/internationalisation_test.py
@@ -1,51 +1,24 @@
 from publicweb.tests.open_consent_test_case import OpenConsentTestCase
 from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
-from django.conf import settings
-from mechanize import ParseString
-from contextlib import contextmanager
+from django.utils import translation
 
 class SettingDoesNotExist:
     pass
 
 class InternationalisationTest(OpenConsentTestCase):
 
-    @contextmanager
-    def patch_settings(**kwargs):
-        from django.conf import settings
-        old_settings = []
-        for key, new_value in kwargs.items():
-            old_value = getattr(settings, key, SettingDoesNotExist)
-            old_settings.append((key, old_value))
-            setattr(settings, key, new_value)
-        yield
-        for key, old_value in old_settings:
-            if old_value is SettingDoesNotExist:
-                delattr(settings, key)
-            else:
-                setattr(settings, key, old_value)
-
     def setUp(self):
-        self.patch_settings(MY_SETTING='my value')
+        translation.activate('de-AT')
         self.login()
         
     def tearDown(self):
-       print "***Language at tear down: ", settings.LANGUAGE_CODE
-           
+        translation.deactivate()
+
     #this test needs to check the table's row's heading
     #At the moment mechanize page pulls out form values not row names
     def test_decision_list_changes(self):
-        print "Language Code: ", settings.LANGUAGE_CODE
-        
         path = reverse('decision_add')
-        page = self.client.get(path)
-
-        forms = ParseString(page.content, '')
-
-        print "***Context Label: ", page.context['decision_form'].fields['short_name'].label        
-        print "***Form Label: ", forms[1].controls[0].get_labels()[0].text
-
-        short_name = page.context['decision_form'].fields['short_name'].label
-        
+        response = self.client.get(path)
+        short_name = response.context['decision_form'].fields['short_name'].label
         self.assertEquals( unicode('Namen'), short_name)
-        
+    
