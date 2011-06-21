fdd05392557b916c3457c942e9dca186b1b00963
paulb <paulb@fen-desktop6>
2011-06-21 16:16:24 +0100

    Began internationalisation
    Change-Id: I117e551307938dc44d106576385e9a858de47863

--- django/openconsent/publicweb/models.py
+++ django/openconsent/publicweb/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.utils.translation import ugettext as _
 import tinymce.models
 import tinymce.widgets
 import datetime
     
 class Decision(models.Model):
-    short_name = models.CharField(max_length=255, verbose_name='Name')
+    short_name = models.CharField(max_length=255, verbose_name=_('Name'))
     decided_date = models.DateField(null=True, blank=True,
-        verbose_name='Decided date')
+        verbose_name=_('Decided date'))
     effective_date = models.DateField(null=True, blank=True,
-        verbose_name='Effective date')
+        verbose_name=_('Effective date'))
     review_date = models.DateField(null=True, blank=True,
-        verbose_name='Review date')
+        verbose_name=_('Review date'))
     expiry_date = models.DateField(null=True, blank=True,
-        verbose_name='Expiry date')
+        verbose_name=_('Expiry date'))
     budget = models.CharField(blank=True, max_length=255,
-        verbose_name='Budget')
+        verbose_name=_('Budget'))
     people = models.CharField(blank=True, max_length=255,
-        verbose_name='People')
+        verbose_name=_('People'))
     description = tinymce.models.HTMLField(blank=True,
-        verbose_name='Description')
+        verbose_name=_('Description'))
     
     def unresolvedconcerns(self):
         answer = "No"
--- django/openconsent/publicweb/tests/__init__.py
+++ django/openconsent/publicweb/tests/__init__.py
@@ -1,4 +1,5 @@
 from publicweb.tests.decisions_test import DecisionsTest
 from publicweb.tests.login_test import LoginTest
 from publicweb.tests.logout_test import LogoutTest
+from publicweb.tests.internationalisation_test import InternationalisationTest
 #from publicweb.tests.open_consent_test_case import OpenConsentTestCase
