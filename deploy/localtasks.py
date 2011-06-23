3c6247d1668639415721aceb624977a3d6c0e9cf
Daniel Levy <daniell@aptivate.org>
2011-06-23 10:37:03 +0100

    Added task to load fixtures. Removed broken import from publicweb/tests/__init__.py
    Change-Id: Ibed7fdf68574685d9ae63fcf352ab0c2e560aa0f

---
+++ deploy/localtasks.py
@@ -0,0 +1,35 @@
+# functions just for this project
+import os
+import subprocess
+import sys
+
+import tasklib
+
+fixtures_to_load = [
+                    'publicweb.decision.json',
+                    'user.json'
+                  ]
+
+fixtures_to_deploy = [
+                    'user.json'
+                     ]
+    
+def deploy(environment=None, svnuser=None, svnpass=None):
+    if environment == None:
+        environment = tasklib._infer_environment()
+
+    tasklib.create_ve()
+    patch_whoosh()
+    tasklib.link_local_settings(environment)
+    tasklib.update_db()
+    link_yaml(environment)
+    load_fixtures()
+
+def load_fixtures():
+    """load fixtures for this environment"""
+    if tasklib._infer_environment() == 'production':
+        fixtures_list = fixtures_to_deploy
+    else:
+        fixtures_list = fixtures_to_load
+    for fixture in fixtures_list:
+        tasklib._manage_py(['loaddata'] + [fixture])
