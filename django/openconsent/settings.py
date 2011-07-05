ea65200359e8c61a65a185cc610b2df51944a7a7
Martin Burchell <martinb@aptivate.org>
2011-07-05 12:55:18 +0100

    Updated instructions to include missing dependency. Removed django.contrib.sites from installed apps. Added local settings file to run the tests quickly using INNODB and MySQL in RAM

--- django/openconsent/settings.py
+++ django/openconsent/settings.py
@@ -115,7 +115,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'publicweb',
