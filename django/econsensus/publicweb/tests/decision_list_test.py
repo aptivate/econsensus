924e48c8a6700f9a20c730fe016059648917ff17
birdsarah <sarah@bonvaya.com>
2012-10-20 09:29:24 -0700

    Refactored DecisionListTest to merge together test_sorts * Reduces the time to run * Uses randomized data * Test excerpt * Remove description test * Randomize feedback
    Notes:
    * think about splitting out methods and create a classSetUp
    * does not test for each page, only proposal, but I think that's sufficient as sorting should work across pages if it works on one

--- django/econsensus/publicweb/tests/decision_list_test.py
+++ django/econsensus/publicweb/tests/decision_list_test.py
@@ -1,7 +1,8 @@
 from django.core.urlresolvers import reverse
 
-import datetime
-import random
+from random import randint
+from string import ascii_letters, digits
+from datetime import date
 
 from decision_test_case import DecisionTestCase
 from publicweb.models import Decision, Feedback
@@ -12,17 +13,8 @@ from lxml.cssselect import CSSSelector
 
 class DecisionListTest(DecisionTestCase):
 
-    def test_decisions_can_be_sorted_by_id(self):
-        db_list = [x.id for x in Decision.objects.filter(organization=self.bettysorg, status='proposal').order_by('id')]
-        response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), {'sort': 'id'})
-        site_list = [x.id for x in response.context['object_list']]
-        self.assertEquals(db_list, site_list)
-
-    def test_decisions_can_be_sorted_by_description(self):
-        db_list = [x.description for x in Decision.objects.filter(organization=self.bettysorg, status='proposal').order_by('description')]
-        response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), {'sort': 'description'})
-        site_list = [x.description for x in response.context['object_list']]
-        self.assertEquals(db_list, site_list)
+    def tearDown(self):
+        Decision.objects.all().delete()
 
     def test_all_sorts_result_in_one_arrow_present(self):
         """Assert only one sort class is present in the decision list view"""
@@ -42,84 +34,89 @@ class DecisionListTest(DecisionTestCase):
                 self.assertEquals(len(sorts), 1, 'Number of sort arrows should be 1. But is ' + str(len(sorts))
                                                  + ' for page=' + page + ' sort_query=' + sort_query)
 
-    def test_list_pages_can_be_sorted_by_date(self):
-        Decision.objects.all().delete()
-        # these are the dates we'll set in the test (also defines number of decisions that get made)
-        random_dates = [datetime.date.fromordinal(random.randint(500000, 800000)) for i in range(3)]
+    def test_list_pages_can_be_sorted(self):
+        """Test that sort works for all Decision List columns we offer it on"""
+        number_of_test_decisions = 3  # Two additional empty decisions will be made
+
+        # these are the dates we'll set in the test
+        random_dates = [self._get_random_date() for i in range(number_of_test_decisions)]
+        random_descriptions = [self._get_random_string(30) for i in range(number_of_test_decisions)]
+
         # set all the columns you want to test sorting on
-        columns = ['decided_date', 'effective_date', 'review_date', 'expiry_date', 'deadline', 'archived_date']
+        date_columns = ['deadline', 'decided_date', 'review_date', 'archived_date']
 
-        # make an initial empty decision
-        self.make_decision(description="Decision None 1", organization=self.bettysorg)
+        #############################
+        # Make the decisions
+        #############################
+        # Make an initial empty decision
+        decisions = []
+        decisions.append(self.make_decision(description="Decision None 1", organization=self.bettysorg))
 
-        # create decisions with dates - add the same date to all the columns
-        for random_date in random_dates:
-            d = self.make_decision(description='Decision %s' % random_date, organization=self.bettysorg)
-            for column in columns:
-                setattr(d, column, random_date)
+        # Create decisions with random data
+        for i in range(number_of_test_decisions):
+            d = self.make_decision(description=random_descriptions[i], organization=self.bettysorg)
+            for column in date_columns:
+                setattr(d, column, random_dates[i])
             d.save()
+            decisions.append(d)
 
-        # make a final empty decision
-        self.make_decision(description="Decision None 2", organization=self.bettysorg)
+        # Set random feedback (offset by 1 so empty decision remains empty)
+        for i in range(1, number_of_test_decisions + 1):
+            for f in range(randint(1, 4)):
+                Feedback(description=self._get_random_string(10), decision=decisions[i], author=self.user).save()
 
-        # we need sorted dates to compare the return values against
-        sorted_dates_forward = sorted(random_dates)
-        sorted_dates_reverse = sorted_dates_forward[::-1]
+        # Make a final empty decision
+        decisions.append(self.make_decision(description="Decision None 2", organization=self.bettysorg))
 
-        for column in columns:
-            response_forward = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), dict(sort=column))
-            object_list_forward = response_forward.context['object_list']
+        # Get the last_modified & id values
+        last_modifieds = [decision.last_modified.replace(microsecond=0) for decision in decisions]
+        ids = [decision.id for decision in decisions]
+        feedbackcounts = [decision.feedbackcount() for decision in decisions]
+        excerpts = [decision.excerpt for decision in decisions]
+        #############################
 
-            response_reverse = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), dict(sort='-' + column))
-            object_list_reverse = response_reverse.context['object_list']
+        # we need sorted values to compare against
+        sorted_dates = sorted(random_dates)
+        sorted_last_modifieds = sorted(last_modifieds)[::-1]  # because we sort in reverse by default
+        sorted_ids = sorted(ids)[::-1]  # because we sort in reverse by default
+        sorted_feedbackcounts = sorted(feedbackcounts)[::-1]  # because we sort in reverse by default
+        sorted_excerpts = sorted(excerpts, key=unicode.lower)
 
-            object_list_length = len(object_list_forward)
-            self.assertEquals(len(object_list_forward), len(object_list_reverse))
+        # Test Dates
+        for column in date_columns:
+            response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), dict(sort=column))
+            object_list = response.context['object_list']
 
-            #Test everything
-            for index, sorted_date_forward in enumerate(sorted_dates_forward):
-                self.assertEquals(sorted_date_forward, getattr(object_list_forward[index], column))
-                self.assertEquals(sorted_dates_reverse[index], getattr(object_list_reverse[index], column))
-            self.assertEquals(None, getattr(object_list_forward[object_list_length - 2], column))
-            self.assertEquals(None, getattr(object_list_forward[object_list_length - 1], column))
-            self.assertEquals(None, getattr(object_list_reverse[object_list_length - 2], column))
-            self.assertEquals(None, getattr(object_list_reverse[object_list_length - 1], column))
+            for index, sorted_date in enumerate(sorted_dates):
+                self.assertEquals(sorted_date, getattr(object_list[index], column), 'Testing date sort failed for' + column)
+            self.assertEquals(None, getattr(object_list[len(object_list) - 2], column), 'Testing date sort failed for' + column)
+            self.assertEquals(None, getattr(object_list[len(object_list) - 1], column), 'Testing date sort failed for' + column)
 
-    def test_list_page_sorted_by_description(self):
-        # Create test decisions in reverse date order.
-        self.make_decision(description="Apple", organization=self.bettysorg)
-        self.make_decision(description="Dandelion", organization=self.bettysorg)
-        self.make_decision(description="Blackberry", organization=self.bettysorg)
-        self.make_decision(description="Coconut", organization=self.bettysorg)
-
-        response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), dict(sort='description'))
-
+        # Test Last Modified
+        response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), {'sort': '-last_modified'})
         object_list = response.context['object_list']
+        for index, sorted_last_modified in enumerate(sorted_last_modifieds):
+            self.assertEquals(sorted_last_modified, getattr(object_list[index], 'last_modified'), 'Testing date sort failed for last_modified')
 
-        self.assertEquals("Apple", getattr(object_list[0], 'description'))
-        self.assertEquals("Blackberry", getattr(object_list[1], 'description'))
-        self.assertEquals("Coconut", getattr(object_list[2], 'description'))
-        self.assertEquals("Dandelion", getattr(object_list[3], 'description'))
-
-    def test_list_page_sorted_by_feedback(self):
-        # Create test decisions in reverse date order.
-        Decision.objects.all().delete()
-        decision1 = self.make_decision(description="Apple", organization=self.bettysorg)
-        decision2 = self.make_decision(description="Coconut", organization=self.bettysorg)
-        decision3 = self.make_decision(description="Blackberry", organization=self.bettysorg)
-        Feedback(description="One", decision=decision1, author=self.user).save()
-        Feedback(description="Two", decision=decision1, author=self.user).save()
-        Feedback(description="Three", decision=decision1, author=self.user).save()
-        Feedback(description="One", decision=decision3, author=self.user).save()
-        Feedback(description="Two", decision=decision3, author=self.user).save()
-
-        response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), dict(sort='feedback'))
-
+        #At this point, the ids in browser are all out of order, so good time to test id sort
+        response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), {'sort': '-id'})
         object_list = response.context['object_list']
+        for index, sorted_id in enumerate(sorted_ids):
+            self.assertEquals(sorted_id, getattr(object_list[index], 'id'), 'Testing id sort failed')
 
-        self.assertEquals(decision1.id, getattr(object_list[0], 'id'))
-        self.assertEquals(decision3.id, getattr(object_list[1], 'id'))
-        self.assertEquals(decision2.id, getattr(object_list[2], 'id'))
+        # Test Feedback
+        response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), {'sort': 'feedback'})
+        object_list = response.context['object_list']
+        for index, sorted_feedbackcount in enumerate(sorted_feedbackcounts):
+            self.assertEquals(sorted_feedbackcount, object_list[index].feedbackcount(), 'Testing feedbackcount sort failed')
+        self.assertEquals(0, object_list[len(object_list) - 2].feedbackcount(), 'Testing feedbackcount sort failed')
+        self.assertEquals(0, object_list[len(object_list) - 1].feedbackcount(), 'Testing feedbackcount sort failed')
+
+        # Test Excerpt
+        response = self.client.get(reverse('publicweb_item_list', args=[self.bettysorg.slug, 'proposal']), {'sort': 'excerpt'})
+        object_list = response.context['object_list']
+        for index, sorted_excerpt in enumerate(sorted_excerpts):
+            self.assertEquals(sorted_excerpt, getattr(object_list[index], 'excerpt'), 'Testing excerpt sort failed')
 
     def test_decision_list_can_be_filtered_by_status(self):
         self.create_decisions_with_different_statuses()
