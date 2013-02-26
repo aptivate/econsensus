edb08a4d7fe97a63cc6385104d479e07b0878a75
jogwen <joanna.wakeford+github@gmail.com>
2013-02-26 17:32:25 +0000

    Fix problems with installing econsensus for first time: - don't override entirety of deploy command in localtasks.py as it will (and has) get out of date. Instead, use main tasklib.deploy function (including git submodule commands) and define only post_deploy commands in localtasks.py - the tests for whether we want to load env-specific data for auth_user and django_sites now take into account:  - the fact that django-guardian specifies a post_syncdb signal handler     to create an User called AnonymousUser, so on first deploy, the table     won't be empty, but does need initializing  - the fact that django.contrib.sites specifies a post_syncdb signal    handler to create an example.com Site object with id=1, so on first     deploy, the table won't be empty, but does need initializing https://aptivate.kanbantool.com/boards/5986-econsensus#tasks-1260285

--- deploy/localtasks.py
+++ deploy/localtasks.py
@@ -1,18 +1,12 @@
 # functions just for this project
-import os, sys
+import json
+import os
+import sys
 import subprocess
 
 import tasklib
 
-def deploy(environment=None, svnuser=None, svnpass=None):
-    if environment == None:
-        environment = tasklib._infer_environment()
-
-    tasklib.create_ve()
-    tasklib.create_private_settings()
-    tasklib.link_local_settings(environment)
-    tasklib.update_db()
-
+def post_deploy(environment=None, svnuser=None, svnpass=None):
     load_auth_user(environment)
     load_django_site_data(environment)
         
@@ -27,13 +21,33 @@ def load_sample_data(environment, force=False):
 
         tasklib._manage_py(['loaddata', "sample_data.json"])
 
+def _get_objects_from_db(app_name, model_name):
+    for output_line in tasklib._manage_py(['dumpdata', "{0}.{1}".format(app_name.lower(), model_name.lower())]):
+        try:
+            data = json.loads(output_line)
+        except ValueError:
+            continue
+        return data
+
+def _get_matching_objects_from_db(app_name, model_name, target_field_name, target_value):
+    data = _get_objects_from_db(app_name, model_name)
+    return [object for object in data if object.get(target_field_name) == target_value]
+
+def _auth_user_needs_initializing():
+    data = _get_objects_from_db('auth', 'User')
+    if not data:
+        return True
+    elif len(data) == 1 and data[0].get('fields', {}).get('username') == u'AnonymousUser':
+        # django-guardian creates User AnonymousUser after syncdb
+        return True
+    else:
+        return False
+
 def load_auth_user(environment, force=False):
     """load auth user fixture based on environment. """
     if force == False:
-        # first check if it has already been loaded
-        output_lines = tasklib._manage_py(['dumpdata', 'auth.user'])
-        if output_lines[0] != '[]':
-            print "Environment '", environment, "' already has auth.user loaded."
+        if not _auth_user_needs_initializing():
+            print "Environment '", environment, "' already has auth.user initialized."
             return
 
     local_fixtures_directory = os.path.join(tasklib.env['django_dir'], 'publicweb',
@@ -45,13 +59,26 @@ def load_auth_user(environment, force=False):
     else:
         tasklib._manage_py(['loaddata', "default_auth_user.json"])
 
+def _site_data_needs_initializing():
+    # We only care about our app's Site object (ie. the one with id=settings.SITE_ID)
+    target_id = 1
+    matching_objects = _get_matching_objects_from_db('sites', 'Site', 'pk', target_id)
+    if len(matching_objects) > 1:
+        raise Exception, "Found more than one Site with id '{0}'".format(target_id)
+    if not matching_objects:
+        return True
+    elif matching_objects[0].get('fields', {}).get('domain', '') == u'example.com':
+        # django.contrib.sites automatically creates a Site with domain 
+        # 'example.com' when the table is created
+        return True
+    else:
+        return False
+
 def load_django_site_data(environment, force=False):
-    """load data for django sites framework. """
+    """Load data for django sites framework. """
     if force == False:
-        # first check if it has already been loaded
-        output_lines = tasklib._manage_py(['dumpdata', 'sites'])
-        if output_lines[0] != '[]':
-            print "Environment '", environment, "' already has site data loaded."
+        if not _site_data_needs_initializing():
+            print "Environment '", environment, "' already has site data initialized."
             return
     local_fixtures_directory = os.path.join(tasklib.env['django_dir'], 'publicweb',
                                            'fixtures')
