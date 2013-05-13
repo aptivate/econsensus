ba15a36a70b9da2b8b7b62a64431e1089e46665f
jogwen <joanna.wakeford+github@gmail.com>
2013-05-13 12:43:08 +0100

    Rebaselined test now that owner of new organization is always the user that created it (not the user whose email is supplied, if there is one).

--- django/econsensus/publicweb/tests/form_test.py
+++ django/econsensus/publicweb/tests/form_test.py
@@ -8,17 +8,20 @@ from decision_test_case import DecisionTestCase
 class FormTest(DecisionTestCase):
     
     def test_owner_of_new_org_is_admin_and_editor(self):
+        """
+        Owner of new organization should be the creating user, and they 
+        should be both admin and editor.
+        """
         new_org_name = new_org_slug = "neworg"
-        new_org_owner = self.charlie
         self.assertEqual(Organization.objects.filter(slug=new_org_slug).count(), 0, "Organization with this slug already exists")
         path = reverse("organization_add")
         response = self.client.post(
             path, 
-            {"name": new_org_name, "slug": new_org_slug, "email": new_org_owner.email}
+            {"name": new_org_name, "slug": new_org_slug, "email": self.charlie.email}
         )
         organizations = Organization.objects.filter(slug=new_org_slug)
         self.assertEqual(organizations.count(), 1, "The Organization wasn't created")
         new_organization = organizations[0]
-        self.assertEqual(new_organization.owner.organization_user.user, new_org_owner, "New Organization has wrong owner")
+        self.assertEqual(new_organization.owner.organization_user.user, self.user, "New Organization has wrong owner")
         self.assertTrue(new_organization.owner.organization_user.is_admin, "Owner of new Organization is not admin")
-        self.assertTrue(new_org_owner.has_perm('edit_decisions_feedback', new_organization), "Owner of new Organization is not editor")
+        self.assertTrue(self.user.has_perm('edit_decisions_feedback', new_organization), "Owner of new Organization is not editor")
