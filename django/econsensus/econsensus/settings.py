3eb328b6e52700fe224ecc0f51e03405edbfabb0
Stephen Paulger <stephen.paulger@newspeak.org.uk>
2013-05-23 01:17:38 +0100

    Add rudimentary haystack search

--- django/econsensus/econsensus/settings.py
+++ django/econsensus/econsensus/settings.py
@@ -149,9 +149,18 @@ INSTALLED_APPS = (
     'remember_me',
     'actionitems',
     'custom_flatpages',
+    'haystack',
 )
 
 ACTIONITEMS_ORIGIN_MODEL = 'publicweb.Decision'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
