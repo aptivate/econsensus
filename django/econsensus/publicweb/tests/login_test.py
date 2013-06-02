629249981010d4d3b0657e1cecf9e83900677afb
jogwen <joanna.wakeford+github@gmail.com>
2013-06-03 00:38:42 +0100

    Added non-fixture tests to login_test.py to test redirect behaviour upon login in all cases.

--- django/econsensus/publicweb/tests/login_test.py
+++ django/econsensus/publicweb/tests/login_test.py
@@ -1,6 +1,10 @@
-from open_consent_test_case import EconsensusFixtureTestCase
 from django.core.urlresolvers import reverse
+from django.test import TestCase
+
 from publicweb.models import Decision
+from publicweb.tests.open_consent_test_case import EconsensusFixtureTestCase
+from publicweb.tests.factories import UserFactory, \
+        OrganizationUserFactory
 
 class LoginTest(EconsensusFixtureTestCase):       
     def test_non_login_is_redirected(self):
@@ -36,3 +40,81 @@ class LoginTest(EconsensusFixtureTestCase):
         path = reverse('publicweb_decision_create', args=[self.bettysorg.slug, Decision.PROPOSAL_STATUS])
         response = self.client.get(path, follow=True)
         self.assertEquals(response.status_code, 200)
+
+
+class LoginTestNonFixture(TestCase):
+    """
+    Uses factories instead of fixtures for faster tests.
+    There's been much confusion over the redirect behaviour upon login, so lets 
+    test all cases here.
+    """
+    def test_single_org_redirect_on_login(self):
+        org_user = OrganizationUserFactory()
+        self.client.logout()
+        org_user.user.set_password('test')
+        org_user.user.save()
+        response = self.client.post(reverse('auth_login'),
+            {'username': org_user.user.username, 'password': 'test'},
+            follow = True)
+        expected_url = reverse('publicweb_item_list', 
+            args=[org_user.organization.slug, Decision.PROPOSAL_STATUS]) 
+        self.assertRedirects(response, expected_url)
+
+    def test_single_org_redirect_on_login_with_root_next_query(self):
+        org_user = OrganizationUserFactory()
+        self.client.logout()
+        org_user.user.set_password('test')
+        org_user.user.save()
+        response = self.client.post(reverse('auth_login')+'?next=/',
+            {'username': org_user.user.username, 'password': 'test'},
+            follow = True)
+        expected_url = reverse('publicweb_item_list', 
+            args=[org_user.organization.slug, Decision.PROPOSAL_STATUS]) 
+        self.assertRedirects(response, expected_url)
+       
+    def test_single_org_redirect_on_login_with_orgadd_next_query(self):
+        org_user = OrganizationUserFactory()
+        self.client.logout()
+        org_user.user.set_password('test')
+        org_user.user.save()
+        response = self.client.post(reverse('auth_login')+'?next=/organizations/add/',
+            {'username': org_user.user.username, 'password': 'test'},
+            follow = True)
+        expected_url = reverse('organization_add') 
+        self.assertRedirects(response, expected_url)
+       
+    def test_multi_org_redirect_on_login(self):
+        user = UserFactory()
+        org_user1 = OrganizationUserFactory(user=user)
+        org_user2 = OrganizationUserFactory(user=user)
+        user.set_password('test')
+        user.save()
+        response = self.client.post(reverse('auth_login'),
+            {'username': user.username, 'password': 'test'},
+            follow = True)
+        expected_url = reverse('organization_list') 
+        self.assertRedirects(response, expected_url)
+       
+    def test_multi_org_redirect_on_login_with_orglist_next_query(self):
+        user = UserFactory()
+        org_user1 = OrganizationUserFactory(user=user)
+        org_user2 = OrganizationUserFactory(user=user)
+        user.set_password('test')
+        user.save()
+        response = self.client.post(reverse('auth_login')+'?next=/',
+            {'username': user.username, 'password': 'test'},
+            follow = True)
+        expected_url = reverse('organization_list') 
+        self.assertRedirects(response, expected_url)
+
+    def test_multi_org_redirect_on_login_with_other_next_query(self):
+        user = UserFactory()
+        org_user1 = OrganizationUserFactory(user=user)
+        org_user2 = OrganizationUserFactory(user=user)
+        user.set_password('test')
+        user.save()
+        response = self.client.post(reverse('auth_login')+'?next=/organizations/add/',
+            {'username': user.username, 'password': 'test'},
+            follow = True)
+        expected_url = reverse('organization_add') 
+        self.assertRedirects(response, expected_url)
