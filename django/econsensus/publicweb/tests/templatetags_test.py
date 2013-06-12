412feb3d66d447a6ed9dc928b8d80ab977717740
jogwen <joanna.wakeford+github@gmail.com>
2013-06-12 15:07:53 +0100

    Converted new test to be speedier.

--- django/econsensus/publicweb/tests/templatetags_test.py
+++ django/econsensus/publicweb/tests/templatetags_test.py
@@ -1,16 +1,16 @@
-from open_consent_test_case import EconsensusFixtureTestCase
+from django.contrib.comments.models import Comment
+from django.test import TestCase
 
 from publicweb.templatetags import publicweb_filters
-from django.contrib.comments.models import Comment
+from publicweb.tests.factories import UserFactory
 
-# test custom template filters
-class CustomTemplateFilterTest(EconsensusFixtureTestCase):
+class CustomTemplateFilterTest(TestCase):
  
     def test_get_user_name_from_comment(self):
-        c = Comment()
-        c.user = self.betty
-        self.assertEqual(publicweb_filters.get_user_name_from_comment(c), "betty")
-        c.user=None
-        self.assertEqual(publicweb_filters.get_user_name_from_comment(c), "An Anonymous Contributor")
-        c.user_name = "Harry"
-        self.assertEqual(publicweb_filters.get_user_name_from_comment(c), "Harry")
+        comment = Comment(user=None, user_name='')
+        self.assertEqual(publicweb_filters.get_user_name_from_comment(comment), "An Anonymous Contributor")
+        comment.user_name = "Harry"
+        self.assertEqual(publicweb_filters.get_user_name_from_comment(comment), "Harry")
+        user = UserFactory()
+        comment.user = user
+        self.assertEqual(publicweb_filters.get_user_name_from_comment(comment), user.username)
