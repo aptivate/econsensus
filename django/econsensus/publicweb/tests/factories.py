433089cb035a435c16d3bfee6c548ec31cd80b7f
jogwen <joanna.wakeford+github@gmail.com>
2013-06-19 14:32:02 +0100

    Merge branch 'develop' of https://github.com/rdn32/econsensus into rdn32-develop for https://github.com/aptivate/econsensus/pull/130
    Conflicts:
        django/econsensus/publicweb/tests/factories.py
        django/econsensus/publicweb/tests/model_test.py

--- django/econsensus/publicweb/tests/factories.py
+++ django/econsensus/publicweb/tests/factories.py
@@ -1,6 +1,8 @@
 import factory
 
 from django.contrib.auth.models import User
+from django.contrib.comments.models import Comment
+from django.contrib.sites.models import Site
 
 from organizations.models import Organization, OrganizationUser,\
     OrganizationOwner
@@ -14,10 +16,8 @@ class UserFactory(factory.DjangoModelFactory):
     # Have factoryboy add a sequenced number, n, to username for uniqueness
     username = factory.Sequence(lambda n: 'user%s' % n)
 
-
 class OrganizationFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Organization
-
 
 class OrganizationUserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = OrganizationUser
@@ -25,16 +25,14 @@ class OrganizationUserFactory(factory.DjangoModelFactory):
     user = factory.SubFactory(UserFactory)
     organization = factory.SubFactory(OrganizationFactory)
 
-
 class OrganizationOwnerFactory(factory.DjangoModelFactory):
     FACTORY_FOR = OrganizationOwner
 
     organization = factory.SubFactory(OrganizationFactory)
-    organization_user = factory.SubFactory(OrganizationUserFactory,
-                                           organization=factory.SelfAttribute(
-                                               '..organization')
-                                           )
-
+    organization_user = factory.SubFactory(
+        OrganizationUserFactory,
+        organization=factory.SelfAttribute('..organization')
+    )
 
 class DecisionFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Decision
@@ -46,3 +44,9 @@ class FeedbackFactory(factory.DjangoModelFactory):
 
     decision = factory.SubFactory(DecisionFactory)
 
+class SiteFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Site
+
+class CommentFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Comment
+    site = factory.SubFactory(SiteFactory)
