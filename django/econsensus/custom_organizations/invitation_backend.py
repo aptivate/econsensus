0f518b40c2605cbbc6d65026444c8ff39ee1bd5c
jogwen <joanna.wakeford+github@gmail.com>
2013-06-26 22:50:29 +0100

    Don't redefine the paths to the templates on CustomInvitationBackend, just put our custom templates in the location the InvitationBackend would expect to find them.

--- django/econsensus/custom_organizations/invitation_backend.py
+++ django/econsensus/custom_organizations/invitation_backend.py
@@ -6,14 +6,10 @@ from custom_organizations.forms import CustomUserRegistrationForm
 
 class CustomInvitationBackend(InvitationBackend):
     """
-    Customise django-organizations' default InvitationBackend so that we can
-    provide our own body and subject templates for the emails.
+    Customise django-organizations' default InvitationBackend so that we can 
+    use our own form so we can fine tune the fields we want and make it look 
+    the same as registration sign up
     """
-    invitation_subject = 'email/invitation_subject.txt'
-    invitation_body = 'email/invitation_body.html'
-    reminder_body = 'email/reminder_body.html'
-    # Use our own form so we can fine tune the fields we want
-    # and make it look the same as registration sign up
     form_class = CustomUserRegistrationForm
 
     def get_success_url(self):
