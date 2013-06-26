0c4cae07d62828f942053064b4c288ef7a6065ec
jogwen <joanna.wakeford+github@gmail.com>
2013-06-26 15:58:41 +0100

    Rationalised the whitespace so that all 4 lines start with 4 spaces, rather than some with 8 spaces and some with indent.

--- django/econsensus/publicweb/registration_signal.py
+++ django/econsensus/publicweb/registration_signal.py
@@ -3,9 +3,9 @@ from registration.signals import user_registered
 
 # we catch the user created signal and add the first_name and last_name
 def user_created(sender, user, request, **kwargs):
-	form = CustomUserSignupRegistrationForm(request.POST)
-        user.first_name = form.data['first_name']
-        user.last_name = form.data['last_name']
-	user.save()
+    form = CustomUserSignupRegistrationForm(request.POST)
+    user.first_name = form.data['first_name']
+    user.last_name = form.data['last_name']
+    user.save()
 
 user_registered.connect(user_created)
