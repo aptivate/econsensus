d26991e5dd848710aa04cbba1a0f403112f14f28
Robin Newton <rnewton@fastmail.fm>
2013-07-08 12:12:53 +0000

    Add page for adding comment to feedback, for use when javascript is disabled. Make the show/hide logic for comment form hinge on "showable" class rather than "form_comment" so that the form is always displayed on comment page. (Fix for https://aptivate.kanbantool.com/boards/5986-econsensus#tasks-1128515)

--- django/econsensus/publicweb/static/js/showhide.js
+++ django/econsensus/publicweb/static/js/showhide.js
@@ -1,23 +1,23 @@
 /*
-Show/hide the feedback_comment class amongst multiple,
+Show/hide the showable class amongst multiple,
 with activation via a show button
 Objects are automatically hidden if the 
 user clicks outside the area
 */
   $(document).ready(function() {
     $("body").live("click", function() {
-      $(".form_comment").hide();
+      $(".showable").hide();
     });
     
     $(".show").live("click", function(e) {
-    var this_comment_form  = $(this).parent().siblings().find(".form_comment");
-      $(".form_comment").hide();
-      this_comment_form.show();
+    var this_showable_thing  = $(this).parent().siblings().find(".showable");
+      $(".showable").hide();
+      this_showable_thing.show();
       e.stopPropagation();
       e.preventDefault();
     });
     
-    $(".form_comment").live("click", function(e) {
+    $(".showable").live("click", function(e) {
       e.stopPropagation();
     });
   });
