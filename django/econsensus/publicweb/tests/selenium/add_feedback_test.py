4bc534d59f6af09a9a4ed4cb0de5fca42f87d66c
Daniel Levy <daniell@aptivate.org>
2013-08-14 10:58:25 +0100

    Add selenium tests for the add feedback from Add a CSS class to uniquely identify feedback cancel button.

---
+++ django/econsensus/publicweb/tests/selenium/add_feedback_test.py
@@ -0,0 +1,95 @@
+from publicweb.tests.selenium.selenium_testcase import SeleniumTestCase
+from guardian.shortcuts import assign_perm
+from django_dynamic_fixture import G
+from organizations.models import Organization
+from publicweb.models import Decision
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+class AddFeedbackTest(SeleniumTestCase):
+    def setUp(self):
+        self.login()
+        self.organization = G(Organization)
+        self.organization.add_user(self.user)
+        assign_perm('edit_decisions_feedback',
+               self.user, self.organization)
+            
+    def test_feedback_form_replaces_add_action_item_button(self):
+        # self.selenium is the name of the web driver for the class
+        decision = G(Decision, organization=self.organization, 
+              author=self.user, editor=self.user)
+        driver = self.selenium
+        driver.get("%s/item/detail/%d/" % (
+           self.live_server_url, decision.id))
+        
+        driver.find_element_by_css_selector("a.button.add_feedback").click()
+        
+        WebDriverWait(driver, 10).until(
+            lambda x: x.find_element_by_id("id_description"))
+        
+        self.assertTrue(
+            self.is_element_present(
+                By.CSS_SELECTOR, "#feedback_add_anchor > form"))
+        self.assertFalse(
+             self.is_element_present(
+                 By.CSS_SELECTOR, "#feedback_add_anchor > .add_feedback"))
+    
+    def test_feedback_form_cancel_recreates_button_without_adding_new_item(self):
+        expected_text = "No feedback yet."
+        
+        decision = G(Decision, organization=self.organization, 
+              author=self.user, editor=self.user)
+        driver = self.selenium
+        driver.get("%s/item/detail/%d/" % (
+           self.live_server_url, decision.id))
+        
+        driver.find_element_by_css_selector("a.button.add_feedback").click()
+        
+        WebDriverWait(driver, 10).until(
+            lambda x: x.find_element_by_id("id_description"))
+        
+        driver.find_element_by_css_selector(".feedback_cancel").click()
+
+        WebDriverWait(driver, 10).until(
+            lambda x: x.find_element_by_css_selector("a.button.add_feedback"))
+        
+        actual_text = driver.find_element_by_css_selector(
+              "ol.feedback_list > li").text
+              
+        self.assertTrue(
+             self.is_element_present(
+                 By.CSS_SELECTOR, "#feedback_add_anchor > .add_feedback"))
+        self.assertFalse(
+            self.is_element_present(
+                By.CSS_SELECTOR, "#feedback_add_anchor > form"))
+        self.assertEqual(expected_text, actual_text)
+    
+    def test_action_item_form_save_with_valid_form_creates_action_item(self):
+        expected_text = ('Consent\ntest says:\ntest\nEdit Comment')
+        
+        decision = G(Decision, organization=self.organization, 
+              author=self.user, editor=self.user)
+        driver = self.selenium
+        driver.get("%s/item/detail/%d/" % (
+           self.live_server_url, decision.id))
+        
+        driver.find_element_by_css_selector("a.button.add_feedback").click()
+        
+        WebDriverWait(driver, 10).until(
+            lambda x: x.find_element_by_id("id_description"))
+        
+        driver.find_element_by_name('description').send_keys("test")
+        selector = Select(driver.find_element_by_name("rating"))
+        selector.select_by_visible_text("consent")
+        
+        driver.find_element_by_css_selector(".button.go.once").click()
+        
+        WebDriverWait(driver, 10).until(
+            lambda x: x.find_element_by_css_selector(".button.add_feedback"),
+            "Check the data being submitted is valid")
+        
+        actual_text = driver.find_element_by_css_selector(
+              "ol.feedback_list > li").text
+        
+        self.assertEqual(expected_text, actual_text)
