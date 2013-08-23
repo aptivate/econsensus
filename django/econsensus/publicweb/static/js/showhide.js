7ed6f370c0abcf8047921c57e42e1a04fb1e38c4
Daniel Levy <daniell@aptivate.org>
2013-08-23 17:15:14 +0100

    Convert cancel links to inputs

--- django/econsensus/publicweb/static/js/showhide.js
+++ django/econsensus/publicweb/static/js/showhide.js
@@ -5,13 +5,25 @@ Objects are automatically hidden if the
 user clicks outside the area
 */
   $(document).ready(function() {
-    $(".controls .button.cancel").on("click", function() {
+    $(".controls input[value=\"Cancel\"]").on("click", function(e) {
       $(".form_comment").hide();
+      e.stopPropagation();
+      e.preventDefault();
     });
+    
+    $(".controls input[value=\"Post\"]").on("click", function(e) {
+    	var this_comment_form  = $(this).parent().parent();
+    	if(!this_comment_form.parsley('validate'))
+    	{
+    		e.stopPropogation();
+    		e.preventDefault();
+    	}
+      });
     
     $(".show").on("click", function(e) {
       var this_comment_form  = $(this).parent().siblings().find(".form_comment");
       $(".form_comment").hide();
+      this_comment_form.parsley()
       this_comment_form.show();
       // Ensure "Post" button is on screen
       this_comment_form.find(".button").focus();
