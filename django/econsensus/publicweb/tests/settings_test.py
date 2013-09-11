de0a9cd42239f7f0cd166cb87bcf611b36615aba
Daniel Levy <daniell@aptivate.org>
2013-09-11 11:38:33 +0100

    Change tests to make them more readable

--- django/econsensus/publicweb/tests/settings_test.py
+++ django/econsensus/publicweb/tests/settings_test.py
@@ -9,12 +9,14 @@ from mock import patch, MagicMock
 from django.test.client import RequestFactory
 from publicweb.views import UserNotificationSettings
 from publicweb.forms import NotificationSettingsForm
+import publicweb
 from django.core.urlresolvers import reverse
 from dbsettings.models import Root
 from django.http import HttpResponseRedirect
 
-def get_organization(**kwargs):
+def create_fake_organization(**kwargs):
     return OrganizationFactory.build(**kwargs)
+   
         
 class SettingsTest(SimpleTestCase):
     def test_notification_settings_have_user_field(self):
@@ -50,245 +52,217 @@ class SettingsTest(SimpleTestCase):
         self.assertEqual(MAIN_ITEMS_NOTIFICATIONS_ONLY, 
          the_settings.notification_level)
     
-    def test_user_notification_settings_view_uses_a_form(self):
-        notification_settings_view = UserNotificationSettings()
-        notification_settings_view.object = MagicMock(spec=NotificationSettings)
-        notification_settings_view.object._meta.fields = []
-        notification_settings_view.object._meta.many_to_many = []
-        
+    @patch('publicweb.views.Organization.objects', 
+           new=MagicMock(
+                 spec=Organization.objects,
+                 get=create_fake_organization,
+                 filter=create_fake_organization
+           )
+    )
+    def test_notification_settings_view_uses_a_form(self):
         user = UserFactory.build(id=1)
-        organization = get_organization()
+        organization = create_fake_organization(id=2)
         
-        organization_objects = MagicMock(Organization.objects)
-        organization_objects.get = get_organization
-        organization_objects.filter = get_organization
+        request = RequestFactory().get('/')
+        request.user = user
         
-        with patch('publicweb.views.Organization.objects', 
-                   new=organization_objects):
-            request = RequestFactory().get('/')
-            request.user = user
-            notification_settings_view.request = request
-            context = notification_settings_view.dispatch(request, 
-              organization=organization.id).context_data
-
+        context = UserNotificationSettings.as_view()(
+                      request, 
+                      organization=organization.id
+        ).context_data
+    
         self.assertIn('form', context)
     
-    def test_user_notifcation_settings_view_redirects_to_organization_list(self):
+    def test_notifcation_settings_view_redirects_to_organization_list(self):
         notification_settings_view = UserNotificationSettings()
         self.assertEqual(reverse('organization_list'), 
              notification_settings_view.get_success_url())
-        
-    def test_user_notification_settings_view_uses_user_notification_settings_form(self):
-        notification_settings_view = UserNotificationSettings()
-        notification_settings_view.object = MagicMock(spec=NotificationSettings)
-        
+    
+    @patch('publicweb.views.Organization.objects', 
+           new=MagicMock(
+                 spec=Organization.objects,
+                 get=create_fake_organization,
+                 filter=create_fake_organization
+           )
+    )
+    def test_notification_settings_view_uses_notification_settings_form(self):
         user = UserFactory.build(id=1)
-        organization = get_organization()
+        organization = create_fake_organization(id=2)
         
-        organization_objects = MagicMock(Organization.objects)
-        organization_objects.get = get_organization
-        organization_objects.filter = get_organization
-        
-        with patch('publicweb.views.Organization.objects', 
-                   new=organization_objects):
-            request = RequestFactory().get('/')
-            request.user = user
-            notification_settings_view.request = request
-            context = notification_settings_view.dispatch(request,
-             organization=organization.id).context_data
+        request = RequestFactory().get('/')
+        request.user = user
+
+        context = UserNotificationSettings.as_view()(
+                        request,
+                        organization=organization.id
+        ).context_data
         
         self.assertIsInstance(context['form'], NotificationSettingsForm)
     
     def test_notification_settings_view_requires_login(self):
-        notification_settings_view = UserNotificationSettings()
         request = RequestFactory().get('/')
         
         user = AnonymousUser()
         
-        organization = get_organization()
+        organization = create_fake_organization(id=2)
         
         request.user = user
         
-        response = notification_settings_view.dispatch(request,
+        response = UserNotificationSettings.as_view()(request,
              organization=organization.id)
         
         self.assertIsInstance(response, HttpResponseRedirect)
     
-    def test_post_valid_data_saves_settings(self):
-        notification_settings_view = UserNotificationSettings()
-        
-        user = UserFactory.build(id=1)
-        organization = get_organization(id=2)
-        
-        organization_objects = MagicMock(Organization.objects)
-        organization_objects.get = get_organization
-        organization_objects.filter = get_organization
-        
-        meta = MagicMock()
-        meta.fields = []
-        meta.many_to_many = []
-        
-        notification_settings = MagicMock(spec=NotificationSettings)
-        notification_settings._meta = meta
-        notification_settings.root_id = None
-        
-        def get_settings(**kwargs):
-            return notification_settings
-        
-        notification_settings_objects = MagicMock(NotificationSettings.objects)
-        notification_settings_objects.get = get_settings
+    @patch('publicweb.views.Organization.objects', 
+           new=MagicMock(
+                spec=Organization.objects,
+                get=create_fake_organization,
+                filter=create_fake_organization
+           )
+    )
+    @patch('publicweb.views.UserNotificationSettings.model',
+           return_value=MagicMock(
+                spec=NotificationSettings,
+                _meta=MagicMock(fields=[], many_to_many=[]),
+                root_id=None
+           )
+    )
+    def test_posting_valid_data_saves_settings(self, settings_obj):
+        organization = create_fake_organization(id=2)
         
         request = RequestFactory().post(
             reverse('notification_settings', args=[organization.id]), 
-            {
-             'notification_level': unicode(NO_NOTIFICATIONS)
-             })
-        
-        request.user = user
-        notification_settings_view.request = request
-        
-        org_objects_attribute = 'publicweb.views.Organization.objects'
-        settings_view_model = 'publicweb.views.UserNotificationSettings.model'
-        settings_view_model_objects = \
-            'publicweb.views.UserNotificationSettings.model.objects'
-        
-        with patch(org_objects_attribute, new=organization_objects):
-            with patch(settings_view_model, new=notification_settings) \
-                as settings_obj:
-                with patch(settings_view_model_objects,
-                       new=notification_settings_objects):
-                    notification_settings_view.dispatch(request, 
-                        organization=organization.id)
-                    
-        self.assertTrue(settings_obj.save.called)
-    
-    def test_post_invalid_data_returns_form_with_errors(self):
-        notification_settings_view = UserNotificationSettings()
+            {'notification_level': unicode(NO_NOTIFICATIONS)}
+        )
         
         user = UserFactory.build(id=1)
-        organization = get_organization(id=2)
+        request.user = user
         
-        organization_objects = MagicMock(Organization.objects)
-        organization_objects.get = get_organization
-        organization_objects.filter = get_organization
+        # This patch depends on the UsertNotificationSettings.model patch 
+        # It needs to return the object created by that patch, which is passed 
+        # in as a parameter.
+        # The only way I've found to handle the dependency is to do this patch
+        # here
+        with patch('publicweb.views.UserNotificationSettings.model.objects',
+                   get=lambda organization, user: settings_obj):
+            UserNotificationSettings.as_view()(
+                request, 
+                organization=organization.id
+            )
         
-        meta = MagicMock()
-        meta.fields = []
-        meta.many_to_many = []
-        
-        notification_settings = MagicMock(spec=NotificationSettings)
-        notification_settings._meta = meta
-        
-        def get_settings(**kwargs):
-            return notification_settings
-        
-        notification_settings_objects = MagicMock(NotificationSettings.objects)
-        notification_settings_objects.get = get_settings
+        self.assertTrue(settings_obj.save.called)
+    
+    @patch('publicweb.views.Organization.objects', 
+           new=MagicMock(
+                spec=Organization.objects,
+                get=create_fake_organization,
+                filter=create_fake_organization
+           )
+    )
+    @patch('publicweb.views.UserNotificationSettings.model',
+           return_value=MagicMock(
+                spec=NotificationSettings,
+                _meta=MagicMock(fields=[], many_to_many=[]),
+                root_id=None
+           )
+    )
+    def test_posting_invalid_data_returns_form_with_errors(self, settings_obj):        
+        user = UserFactory.build(id=1)
+        organization = create_fake_organization(id=2)
         
         request = RequestFactory().post(
             reverse('notification_settings', args=[organization.id]))
         
         request.user = user
-        notification_settings_view.request = request
         
-        org_objects_attribute = 'publicweb.views.Organization.objects'
-        settings_view_model = 'publicweb.views.UserNotificationSettings.model'
-        settings_view_model_objects = \
-            'publicweb.views.UserNotificationSettings.model.objects'
-        
-        with patch(org_objects_attribute, new=organization_objects):
-            with patch(settings_view_model, new=notification_settings):
-                with patch(settings_view_model_objects,
-                       new=notification_settings_objects):
-                    response = notification_settings_view.dispatch(request, 
-                    organization=organization.id)
+        # This patch depends on the UsertNotificationSettings.model patch 
+        # It needs to return the object created by that patch, which is passed 
+        # in as a parameter.
+        # The only way I've found to handle the dependency is to do this patch
+        # here
+        with patch('publicweb.views.UserNotificationSettings.model.objects',
+                   get=lambda organization, user: settings_obj):
+            response = UserNotificationSettings.as_view()(
+                request, 
+                organization=organization.id
+            )
+            
         self.assertIn('form', response.context_data)
         self.assertTrue(response.context_data['form'].errors)
     
-    def test_form_valid_method_gives_new_settings_a_root_object(self):
+    @patch('publicweb.views.Root.objects', spec=Root.objects)
+    def test_form_valid_method_creates_a_root_object(self, root):
         """
         For some reason, settings classes need a "root_id". 
         This should be added in the form valid method, if it doesn't already
         exist 
         """
-        notification_settings_view = UserNotificationSettings()
         
         user = UserFactory.build(id=1)
-        organization = get_organization(id=2)
+        organization = create_fake_organization(id=2)
         
-        notification_settings_instance = MagicMock(spec=NotificationSettings)
+        notification_settings_instance = MagicMock(
+                spec=NotificationSettings,
+                organisation=organization,
+                user=user,
+                root_id=None,
+                meta = MagicMock(fields=[], many_to_many=[])
+        )
         
-        meta = MagicMock()
-        meta.fields = []
-        meta.many_to_many = []
-        
-        notification_settings_instance.organisation = organization
-        notification_settings_instance.user = user
-        notification_settings_instance._meta = meta
-        notification_settings_instance.root_id = None
-        
-        data = {'notification_level': NO_NOTIFICATIONS}
-        
-        notification_settings_form = NotificationSettingsForm(data,
-          instance=notification_settings_instance)
-        
-        root_objects = MagicMock(spec=Root.objects)
+        notification_settings_form = NotificationSettingsForm(
+                {'notification_level': NO_NOTIFICATIONS},
+                instance=notification_settings_instance
+        )
         
         request = RequestFactory().post(
-            reverse('notification_settings', args=[organization.id]))
+            reverse('notification_settings', args=[organization.id])
+        )
         
         request.user = user
-        notification_settings_view.request = request
-
-        with patch('publicweb.views.Root.objects', root_objects) as root:
-            notification_settings_view.form_valid(notification_settings_form)
+        
+        notification_settings_view = UserNotificationSettings()
+        notification_settings_view.request = request    
+        notification_settings_view.form_valid(notification_settings_form)
+        
         self.assertTrue(root.create.called)
     
-    def test_cancel_doesnt_save_settings(self):
-        notification_settings_view = UserNotificationSettings()
-        
+    @patch('publicweb.views.Organization.objects', 
+           new=MagicMock(
+                spec=Organization.objects,
+                get=create_fake_organization,
+                filter=create_fake_organization
+           )
+    )
+    @patch('publicweb.views.UserNotificationSettings.model',
+           return_value=MagicMock(
+                spec=NotificationSettings,
+                _meta=MagicMock(fields=[], many_to_many=[]),
+                root_id=None
+           )
+    )
+    def test_cancel_doesnt_save_settings(self, settings_obj):
         user = UserFactory.build(id=1)
-        organization = get_organization(id=2)
-        
-        organization_objects = MagicMock(Organization.objects)
-        organization_objects.get = get_organization
-        organization_objects.filter = get_organization
-        
-        meta = MagicMock()
-        meta.fields = []
-        meta.many_to_many = []
-        
-        notification_settings = MagicMock(spec=NotificationSettings)
-        notification_settings._meta = meta
-        notification_settings.root_id = None
-        
-        def get_settings(**kwargs):
-            return notification_settings
-        
-        notification_settings_objects = MagicMock(NotificationSettings.objects)
-        notification_settings_objects.get = get_settings
+        organization = create_fake_organization(id=2)
         
         request = RequestFactory().post(
             reverse('notification_settings', args=[organization.id]), 
             {
-             'notification_level': unicode(NO_NOTIFICATIONS),
-             'submit': "Cancel"
-             })
+                'notification_level': unicode(NO_NOTIFICATIONS),
+                'submit': "Cancel"
+             }
+        )
         
-        request.user = user
-        notification_settings_view.request = request
+        request.user = user         
         
-        org_objects_attribute = 'publicweb.views.Organization.objects'
-        settings_view_model = 'publicweb.views.UserNotificationSettings.model'
-        settings_view_model_objects = \
-            'publicweb.views.UserNotificationSettings.model.objects'
-        
-        with patch(org_objects_attribute, new=organization_objects):
-            with patch(settings_view_model, new=notification_settings) \
-                as settings_obj:
-                with patch(settings_view_model_objects,
-                       new=notification_settings_objects):
-                    notification_settings_view.dispatch(request, 
-                        organization=organization.id)
+        # This patch depends on the UsertNotificationSettings.model patch 
+        # It needs to return the object created by that patch, which is passed 
+        # in as a parameter.
+        # The only way I've found to handle the dependency is to do this patch
+        # here
+        with patch('publicweb.views.UserNotificationSettings.model.objects',
+                    get=lambda organization, user: settings_obj):
+            UserNotificationSettings.as_view()(
+                request, organization=organization.id
+            )
                     
         self.assertFalse(settings_obj.save.called)
