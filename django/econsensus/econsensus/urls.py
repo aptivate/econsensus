4530543fb6f25de26beaefa63966c576b6e65f2a
Hamish Downer <hamish@aptivate.org>
2013-10-01 11:36:01 +0100

    whitespace changes to urls.py

--- django/econsensus/econsensus/urls.py
+++ django/econsensus/econsensus/urls.py
@@ -1,17 +1,16 @@
-from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.core.urlresolvers import reverse_lazy
 from organizations.backends import invitation_backend
-from registration.forms import RegistrationFormUniqueEmail
 from custom_auth.forms import CustomAuthenticationForm, CustomPasswordResetForm
 from registration.backends.default.views import RegistrationView
 from custom_organizations.forms import CustomUserSignupRegistrationForm
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/logout/$', 'django.contrib.auth.views.logout_then_login',
         {'login_url': reverse_lazy('admin:index')}),
@@ -21,15 +20,17 @@ urlpatterns = patterns('',
     url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^organizations/', include('custom_organizations.urls')),
     url(r'^invitations/', include(invitation_backend().get_urls())),
+    # the pages adds a / at the start, so don't put the / after pages
     url(r'^pages', include('django.contrib.flatpages.urls')),
     url(r'', include('publicweb.urls')),
 )
 
-urlpatterns += patterns('',
+urlpatterns += patterns(
+    '',
     url(r'^accounts/register/$', RegistrationView.as_view(
-        form_class = CustomUserSignupRegistrationForm,
-        template_name = 'organizations/register_form.html'),
-        name = 'registration_register'),
+        form_class=CustomUserSignupRegistrationForm,
+        template_name='organizations/register_form.html'),
+        name='registration_register'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
     url(r'^accounts/login/$', 'remember_me.views.remember_me_login',
         {'authentication_form': CustomAuthenticationForm},
