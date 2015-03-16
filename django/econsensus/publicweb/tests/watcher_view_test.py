2ebfe9671540464501dfd965a13ea956ff332eac
Hamish Downer <hamish@aptivate.org>
2015-03-16 18:01:16 +0000

    get watcher_view_tests running again

--- django/econsensus/publicweb/tests/watcher_view_test.py
+++ django/econsensus/publicweb/tests/watcher_view_test.py
@@ -1,28 +1,30 @@
+from django.contrib.auth.models import User
+from django.test.client import RequestFactory
 from django.test.testcases import SimpleTestCase
 from django_dynamic_fixture import N
-from publicweb.models import Decision
-from django.test.client import RequestFactory
-from django.contrib.auth.models import User
-from publicweb.views import AddWatcher, RemoveWatcher
 from mock import patch, MagicMock
 from signals.management import DECISION_CHANGE
 
+from publicweb.models import Decision
+from publicweb.views import AddWatcher, RemoveWatcher
+
+
 class WatcherViewTests(SimpleTestCase):
-    
+
     @patch('publicweb.views.notification')
     def test_add_watcher_view_adds_observer_to_item(self, notifications):
         decision = N(Decision)
         user = N(User)
-        
+
         mock_view = MagicMock(spec=AddWatcher)
         mock_view.get_object = lambda: decision
         mock_view.get_user = lambda: user
         mock_view.get = AddWatcher.get
-        
+
         mock_view.get(mock_view, RequestFactory().get('/', {'next': '/'}))
-        notifications.observe.assert_called_with(decision, user, 
-          DECISION_CHANGE)
-    
+        notifications.observe.assert_called_with(
+            decision, user, DECISION_CHANGE)
+
     @patch("publicweb.views.Decision.objects")
     def test_get_object_tries_to_get_decision(self, decisions):
         view = AddWatcher()
@@ -30,25 +32,25 @@ class WatcherViewTests(SimpleTestCase):
         view.kwargs = {'decision_id': 1}
         view.get_object()
         decisions.get.assert_called_with(pk=1)
-        
+
     def test_get_user_looks_for_user_in_request(self):
         user = N(User)
         request = RequestFactory().get('')
         request.user = user
         view = AddWatcher()
         view.request = request
-        
+
         self.assertEqual(user, view.get_user())
-    
+
     @patch('publicweb.views.notification')
     def test_remove_watcher_view_removes_observer_from_item(self, notifications):
         decision = N(Decision)
         user = N(User)
-        
+
         mock_view = MagicMock(spec=RemoveWatcher)
         mock_view.get_object = lambda: decision
         mock_view.get_user = lambda: user
         mock_view.get = RemoveWatcher.get
-        
+
         mock_view.get(mock_view, RequestFactory().get('/', {'next': '/'}))
         notifications.stop_observing.assert_called_with(decision, user)
